0b0ac8d3353f4ae602cb4bd201ad48ca4a4c8be0
GauriVispute <gaurivispute92@gmail.com>
2024-08-06 21:17:28 +0530

    java files

---
+++ Arrays/Linear_search.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Linear_search {
+    public static int find_key_index(int number[],int key){
+        for (int i =0;i<number.length;i++){
+            if(number[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int number [] = {2,4,6,8,10,12,14,16};
+        int key = 15;
+        System.out.println("Key index in array number = "+find_key_index(number,key));;
+
+    }
+}
