bb1d2987288ca901c515b585d1264a0da631a19b
GauriVispute <gaurivispute92@gmail.com>
2024-08-28 00:31:15 +0530

    java files

--- Arrays/Array_practice/Array_operations.java
+++ Arrays/Array_practice/Array_operations.java
@@ -16,25 +16,37 @@ public class Array_operations {
         System.out.println("2] Deleting an element");
         System.out.println("3] Travers an array");
     }
+
+    public static void traverse(int n []){
+        for (int i =0 ;i<size;i++)
+           System.out.print(n[i]+" "); 
+    }
  
-    @SuppressWarnings("unused")
+    
     public static void Insert(int n[]){
 
-        System.out.println("Enter number ");
+        System.out.print("Enter number ");
         int number = sc.nextInt();
 
-        System.out.println("Enter the index :");
+        System.out.print("Enter the index :");
         index = sc.nextInt();
-
-        
-
 
         // element at beginning (head)
         if(index == 0){
-            size++;
+          /*
+          n = array size 
+          so size is n+1
+           */  
+          size++;
+          
+          for(int i=size;i>index;i--){
+           n[i] = n[i-1];   //n[5]=n[4]
+          } 
+
+            n[index] = number;
 
         }
-
+        traverse(n);
         //element at end (tail)
 
         // element in  in-between
@@ -43,7 +55,16 @@ public class Array_operations {
 
 
     public static void main(String[] args) {
-        System.out.println(index);
+        
+        System.out.print("Enter size of array : ");
+        size = sc.nextInt();
+        int number[] = new int [size+1] ;
+        System.out.print("Enter "+size+" number : ");
+        for (int i =0 ;i<size;i++){
+            number[i] = sc.nextInt();
+        }
+
+        Insert(number);
     }
 
 }
