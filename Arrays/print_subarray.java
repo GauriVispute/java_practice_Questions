5ccefa905eb26fd718365ed6fc0e2d69b805a234
GauriVispute <gaurivispute92@gmail.com>
2024-09-27 23:10:54 +0530

    java files

--- Arrays/print_subarray.java
+++ Arrays/print_subarray.java
@@ -4,8 +4,8 @@ public class print_subarray {
     public static void printSubarray(int n[]){
         for(int i =0;i<n.length;i++){
             int start = i;
-            System.out.print("("+n[start]+")");
-            for(int j =i+1;j<n.length;j++){
+          //  System.out.print("("+n[start]+")");
+            for(int j =i;j<n.length;j++){
                 int end = j;
                 System.out.print("(");
                 for(int k =start;k<=end;k++){
