88be267320ed35d33c8ca8c6de0e38384474fa84
GauriVispute <gaurivispute92@gmail.com>
2024-09-28 20:09:33 +0530

    java file

--- Arrays/print_subarray_and_sum.java
+++ Arrays/print_subarray_and_sum.java
@@ -3,6 +3,15 @@ package Arrays;
 public class print_subarray_and_sum {
 
     public static void  printSubarraySum(int n[]){
+        /*
+         * 1st finding the subarray 
+         * then finding the 'Sum of array'
+         * find the Max and Min numbers of sum
+         * find the total_subarray(ts)  
+        */
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int ts = 0;
 
         for(int i =0 ;i<n.length;i++){
             int start = i;
@@ -13,11 +22,19 @@ public class print_subarray_and_sum {
                 for(int k=start;k<=end;k++){
                     System.out.print(n[k]+" ");
                     sum += n[k];
+                    if (sum>max){
+                        max = sum;
+                    }
+                    if(sum<min)
+                        min = sum;
+                    }
                 }System.out.println(")"+" Sum = "+sum);
+                 ts+=1;
             }
             System.out.println();
         }
-
+        System.out.println("Total Subarray = "+ts);
+        System.out.print("Maximum = "+max+" , Minimum = "+min);
 
     }
 
