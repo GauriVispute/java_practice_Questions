3f54c28a9cff4e9280f07a2dd667a1d4b8a99ace
GauriVispute <gaurivispute92@gmail.com>
2024-10-09 21:34:19 +0530

    java file

---
+++ Arrays/max_subarray_sum.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class max_subarray_sum {
+
+    public static void maxSubarraySum(int n[]){
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0;i<n.length;i++){
+            int start = i;
+            for(int j = i;j<n.length;j++){
+               int end = j;
+               int sum = 0 ;
+               for(int k = start;k<=end;k++){
+                    //System.out.print(n[k]+" ");
+                    //need to calculate the subarray which has maximum sum and print the sum only
+                    sum += n[k];
+               }
+               System.out.println(" Sum = "+sum); 
+                if(sum > max){
+                    max = sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("max Value : "+max);
+    }
+
+    public static void main(String[] args) {
+        int numbers [] = {1,-2,6,-1,3};
+        maxSubarraySum(numbers);
+    }
+}
