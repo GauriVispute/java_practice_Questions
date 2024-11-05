75f02db148f019ab0f343ccf27d0bfe01374fd42
GauriVispute <gaurivispute92@gmail.com>
2024-11-05 21:10:56 +0530

    java file

--- Arrays/Array_practice/Second_largest_element_in_array.java
+++ Arrays/Array_practice/Second_largest_element_in_array.java
@@ -1,26 +1,32 @@
 package Arrays.Array_practice;
 
+import java.util.*;
+
 public class Second_largest_element_in_array {
 
     public static int secondLargestNumber(int [] n){
         int  max = n[0];
         int secondMax = Integer.MIN_VALUE;
 
-        for(int i =0;i<n.length;i++){
+        for(int i =1;i<n.length;i++){
             if(max<n[i]){
                 //max = n[i];
                 secondMax = max;
                 max = n[i];
-                if(max<secondMax){
-                    break;
-                }
+            }else if(n[i]>secondMax && max!=n[i]){
+                secondMax = n[i];
+            }else{//duplicate case
+                secondMax=-1;
             }
+            
         }
         return secondMax;
     }
 
     public static void main(String[] args) {
-        int number [] = {23,1,8,7,66};
+
+        int number [] = {34,2,1,67,40};
+        
         System.out.println(secondLargestNumber(number));
     }
 }
---
+++ Arrays/Array_practice/check_sorted_array.java
@@ -0,0 +1,10 @@
+package Arrays.Array_practice;
+
+public class check_sorted_array {
+
+    public 
+
+    public static void main(String[] args) {
+        
+    }
+}
