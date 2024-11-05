9c2f1f421df7fd44a220ed23e2b5a7f41573eb12
GauriVispute <gaurivispute92@gmail.com>
2024-11-05 21:21:36 +0530

    java file

--- Arrays/Array_practice/check_sorted_array.java
+++ Arrays/Array_practice/check_sorted_array.java
@@ -2,9 +2,22 @@ package Arrays.Array_practice;
 
 public class check_sorted_array {
 
-    public 
+    public static boolean sorted_array(int [] n){
+        boolean flag = true;
+        for(int i = 0;i<n.length-1;i++){
+            if(n[i]>n[i+1]){
+                flag = false;
+            }
+        }
+        return flag;
+    }
 
     public static void main(String[] args) {
-        
+        int numbers [] = {1,4,5,8,2,7};
+        if(sorted_array(numbers) != true){
+            System.out.println("Array is not sorted");
+        }else{
+            System.out.println("Array is sorted");
+        }
     }
 }
