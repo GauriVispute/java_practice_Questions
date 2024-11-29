e416841f036c96e4f074fddd84b3a2fb16e27f65
GauriVispute <gaurivispute92@gmail.com>
2024-11-29 13:22:57 +0530

    java file

---
+++ Arrays/Array_practice/trapped_rainwater.java
@@ -0,0 +1,37 @@
+package Arrays.Array_practice;
+
+public class trapped_rainwater {
+
+    public static int trappedRainwater(int h[]){
+        //case 3 : atleast 3 bars with one samll bar in 2 unequal bars
+        int n = h.length;
+        int leftMax [] = new int [h.length];
+        int rightMax[] = new int [h.length];
+        // calculate leftmax boundary
+        leftMax[0] = h[0];
+        for(int i=1;i<leftMax.length;i++){
+            leftMax[i]=Math.max(h[i],leftMax[i-1]);
+        }
+
+        //calculate rightmax Boundary
+        rightMax[n-1] = h[n-1] ;
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = Math.max(h[i],rightMax[i+1]);
+        }
+
+        int trapped_water = 0;
+        for(int i = 0;i<n;i++){
+            //calculate waterlevel
+            int waterLevel = Math.min(leftMax[i],rightMax[i]);
+            //calculate trapped_water = waterLevel-height(bars);
+            trapped_water += waterLevel - h[i];
+        }
+        return trapped_water ;
+
+    }
+
+    public static void main(String[] args) {
+        int height [] = {4,2,0,6,3,2,5};
+        System.out.println("Trapped rainwater : "+trappedRainwater(height));
+    }
+}
--- Arrays/tapped_rainwater.java
+++ Arrays/tapped_rainwater.java
@@ -14,9 +14,8 @@ public class tapped_rainwater {
             }else{
                 leftMaxBoundary[i] = max;
             }
-           // System.out.print(leftMaxBoundary[i]+" ");
         }
-        //System.out.println();
+
         max = Integer.MIN_VALUE;
         int len = h.length;
         for(int i=0;i<len;i++){
@@ -27,27 +26,16 @@ public class tapped_rainwater {
                 rightMaxBoundary[h.length-1-i] = max;
             }
         }
+       
+       int trapped_water = 0;
+        for(int i = 0;i<h.length;i++){
+           // calculating the minimum of both leftMaxBoundary and rightMaxBoundary
+            int waterlevel= Math.min(leftMaxBoundary[i],rightMaxBoundary[i]);
 
-        // for(int i = 0;i<h.length;i++){
-        //     System.out.print(rightMaxBoundary[i]+" ");
-        // }
-
-        // calculating the minimum of both leftMaxBoundary and rightMaxBoundary
-        int minimumBoundary[] = new int [h.length];
-        for(int i = 0;i<minimumBoundary.length;i++){
-            minimumBoundary[i]= Math.min(leftMaxBoundary[i],rightMaxBoundary[i]);
-           // System.out.print(minimum[i]+" ");
+           //calculating tapped water = waterlevel - height(bars);
+             trapped_water += (waterlevel - h[i]);
         }
-
-        // trapped_water= waterlevel-height;
-        int trapped_water[] = new int [h.length];
-        int trappedWater = 0; 
-        for(int i = 0;i<trapped_water.length;i++){
-           trapped_water[i] =  minimumBoundary[i]-h[i];
-           trappedWater+=trapped_water[i];
-        }
-System.out.println("Trapped rainwater : "+trappedWater);
-
+        System.out.println("Trapped rainwater : "+trapped_water);
     }
     public static void main(String[] args) {
         int height [] = {4,2,0,6,3,2,5};
