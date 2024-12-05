cba5d60caa151f3d0fda2d5b8389ec8419bd950e
GauriVispute <gaurivispute92@gmail.com>
2024-12-05 06:23:26 +0530

    java file

---
+++ Arrays/sortingAlgorithms/counting_sort.java
@@ -0,0 +1,40 @@
+package Arrays.sortingAlgorithms;
+
+public class counting_sort {
+
+    public static void countingSort(int n[]){
+        
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<n.length;i++){
+            max = Math.max(max,n[i]);
+        }
+
+        // find frquency of each number
+        int count [] = new int [max+1];// Because starts from 0//
+        for(int i = 0;i<n.length;i++){
+            count[n[i]]++;
+        }
+
+        //sorting
+        int j =0;
+        for(int i=0;i<n.length;i++){
+            while(count[i]>0){
+                n[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArray(int n[]){
+        for(int i =0;i<n.length;i++){
+            System.out.print(n[i]+" ");
+        }
+    } 
+
+    public static void main(String[] args) {
+        int arr[] = {1,4,1,3,2,4,3,7};
+        countingSort(arr);
+        printArray(arr);
+    }
+}
---
+++ Arrays/sortingAlgorithms/inbuildSortTechniques.java
@@ -0,0 +1,31 @@
+package Arrays.sortingAlgorithms;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class inbuildSortTechniques {
+
+    public static void printArray(Integer arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        //Arrays package
+        int numbers [] = {5,4,1,2,3};  // t.c :- O(n*logn) 
+        Arrays.sort(numbers);
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+    
+        // Collection framework
+        Integer arr[] = {5,4,1,2,3};
+        Arrays.sort(arr,Collections.reverseOrder());
+        printArray(arr);
+
+    }
+
+}
