423cfb3e4a3feb32d13656fc67ca9da4ec624687
GauriVispute <gaurivispute92@gmail.com>
2025-01-02 11:36:52 +0530

    java file

--- recursion/tiling_problem.java
+++ recursion/tiling_problem.java
@@ -5,13 +5,13 @@ public class tiling_problem {
         if(n ==0 || n==1){
             return 1;
         }
-        //choice 
-        //vertical
-        int fnm1 = tilingProblem(n-1);
-        //horizontial
-        int fnm2 = tilingProblem(n-2);
+        // //choice 
+        // //vertical
+        // int fnm1 = tilingProblem(n-1);
+        // //horizontial
+        // int fnm2 = tilingProblem(n-2);
 
-        return fnm1+fnm2;
+        return tilingProblem(n-1)+tilingProblem(n-2);
     }
     public static void main(String[] args) {
         System.out.println(tilingProblem(4));
