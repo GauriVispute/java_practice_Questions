37c16f9b723560ec35c2cd797879de9fee41a0dd
GauriVispute <gaurivispute92@gmail.com>
2025-01-03 21:14:21 +0530

    java file

--- recursion_practice/fibonacci_series.java
+++ recursion_practice/fibonacci_series.java
@@ -1,15 +1,14 @@
 package recursion_practice;
 public class fibonacci_series {
-    public static int fib(int n){
-        if(n==0 || n==1){
-            return n;
+    public static void  fib(int n,int a,int b){
+        if(n>-1){
+            System.out.print(a+" ");
+            fib(n-1,b,a+b);
         }
-        ans = 
-        System.out.println();
     }
 
 
     public static void main(String[] args) {
-        fib(5);
+        fib(5,0,1);
     }
 }
--- recursion_practice/reverse_a_string.java
+++ recursion_practice/reverse_a_string.java
@@ -1,3 +1,20 @@
+package recursion_practice;
+
 public class reverse_a_string {
-    
+    public static void reverse(String str,int i,StringBuilder sb){
+        if(i == -1){
+            System.out.println(sb.toString());
+            return ;
+        }
+        //kaam
+        sb.append(str.charAt(i));
+        
+        reverse(str,i-1,sb);
+        
+
+    }
+    public static void main(String[] args) {
+        String str = "gauri";
+        reverse(str,str.length()-1,new StringBuilder(""));
+    }
 }
