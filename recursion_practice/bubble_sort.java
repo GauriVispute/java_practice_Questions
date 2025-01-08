f4df4ebe553f48d1d4f614239c56c1d13267b443
GauriVispute <gaurivispute92@gmail.com>
2025-01-08 17:03:28 +0530

    java file

--- recursion_practice/bubble_sort.java
+++ recursion_practice/bubble_sort.java
@@ -1,11 +1,11 @@
 package recursion_practice;
 public class bubble_sort {
 
-    public static void swap(int n1,int n2){
-        int temp = n1;
-        n1 = n2;
-        n2 = temp;
-    }
+    // public static void swap(int n1,int n2){
+    //     int temp = n1;
+    //     n1 = n2;
+    //     n2 = temp;
+    // }
 
     public static void bubbleSort(int arr[],int i){
         if(i == arr.length-1){
@@ -13,7 +13,9 @@ public class bubble_sort {
         }
         for(int j = 0;j<arr.length-1-i;j++){
             if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
         bubbleSort(arr,i+1);
---
+++ recursion_practice/selection_sort_by_recursion.java
@@ -0,0 +1,19 @@
+package recursion_practice;
+
+public class selection_sort_by_recursion {
+
+    public static void selectionSort(int arr[],int i){
+        inr minPos = i;
+        for(int j=j+i;j<arr.length;j++){
+            if(arr[minPos]>arr[j]){
+                misPos=j;
+            }
+        }
+        // swap
+        int temp = arr[minPos];
+        arr[minPos] = arr[]
+    }
+    public static void main(String[] args) {
+        
+    }
+}
