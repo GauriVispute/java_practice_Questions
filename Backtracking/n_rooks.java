e7e315783618de60a1970a45626f5dfa187c9f60
GauriVispute <gaurivispute92@gmail.com>
2025-01-17 12:00:08 +0530

    java file

---
+++ Backtracking/n_rooks.java
@@ -0,0 +1,51 @@
+package Backtracking;
+
+public class n_rooks {
+    public static void printBoard(char board[][]){
+        System.out.println("---------chess board-------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean isSafe(char board[][],int row,int col){
+        // towards up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'R'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void nRooks(char board [][],int row){
+       if(row == board.length){
+            printBoard(board);
+            return;
+       }
+       
+        //possiblle places solution
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j] = 'R';
+                nRooks(board,row+1);
+                board[row][j] = 'X';
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char [n][n];
+        //initialization
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = 'x';
+            }
+            System.out.println();
+        }
+        nRooks(board , 0);
+        printBoard(board);
+    }
+}
---
+++ Backtracking_practice/n_queen.java
@@ -0,0 +1,7 @@
+package Backtracking_practice;
+public class n_queen {
+
+    public static void main(String[] args) {
+        
+    }
+}
